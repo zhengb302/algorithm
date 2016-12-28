cfabc9ce08df86cc80e36389ba6e281c8d6a7dd3
zhengluming <908235332@qq.com>
2016-12-28 11:54:22 +0800

    Graph

---
+++ src/algorithm/graph/Graph.java
@@ -0,0 +1,14 @@
+package algorithm.graph;
+
+/**
+ * Graph
+ *
+ * @author zhengluming <luming.zheng@shandjj.com>
+ */
+public class Graph {
+    
+    private int numOfVertex;
+
+    private int numOfEdge;
+    
+}
