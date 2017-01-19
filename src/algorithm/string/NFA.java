07edebff9ca84041410f51e561856ba92fef9892
zhengluming <908235332@qq.com>
2017-01-19 17:44:05 +0800

    regular expression

---
+++ src/algorithm/string/NFA.java
@@ -0,0 +1,10 @@
+package algorithm.string;
+
+/**
+ * 非确定的有穷状态自动机
+ *
+ * @author zhengluming <luming.zheng@shandjj.com>
+ */
+public class NFA {
+
+}
---
+++ src/algorithm/string/RegularExpression.java
@@ -0,0 +1,10 @@
+package algorithm.string;
+
+/**
+ * 正则表达式类
+ *
+ * @author zhengluming <luming.zheng@shandjj.com>
+ */
+public class RegularExpression {
+
+}
